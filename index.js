6c1c5742396a325c39c85e01a35a9c63702cd124
Mathieu Garcia <mgarcia@wiseflat.com>
2014-12-14 23:48:54 +0100

    update core

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ wt._init(function(error, xpl) {
         wt.getLocalConfig();
         
         xpl.on("xpl:email.basic", function(evt) {
-		console.log("Receive message ", evt);
+		console.log("Receive message email.basic ", evt);
                 if(evt.headerName == 'xpl-cmnd' && wt.validEmailBasicSchema(evt.body)) wt.sendEmail(evt.body);
         }); 
         
         xpl.on("xpl:email.config", function(evt) {
-		console.log("Receive message ", evt);
+		console.log("Receive message email.config ", evt);
                 if(evt.headerName == 'xpl-cmnd' && wt.validEmailConfigSchema(evt.body)) wt.setConfig(evt.body);
         }); 
 
         xpl.on("xpl:email.request", function(evt) {
-		console.log("Receive message ", evt);
+		console.log("Receive message email.request ", evt);
                 if(evt.headerName == 'xpl-cmnd') wt.readConfig();
         });
 });
