f9414c81cda392572250d08156a5d3939805d063
Mathieu Garcia <mgarcia@wiseflat.com>
2015-01-09 23:03:22 +0100

    Upgrade core

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 var xplemail = require("./lib/xpl-email");
 
 var wt = new xplemail(null, {
-	xplSource: 'bnz-email.wiseflat'
+	//xplSource: 'bnz-email.wiseflat'
 });
 
-wt._init(function(error, xpl) {
+wt.init(function(error, xpl) {
 
 	if (error) {
 		console.error(error);
 		return;
 	}
         
-        wt.getLocalConfig();
+        // Load config file into hash
+        wt.readConfig();
+        
+        // Send every minutes an xPL status message 
+        setInterval(function(){
+                wt.sendConfig();
+        }, 60 * 1000);
+        
+        xpl.on("xpl:email.request", function(evt) {
+                if(evt.headerName == 'xpl-cmnd') wt.readConfig();
+        });
         
         xpl.on("xpl:email.basic", function(evt) {
-		console.log("Receive message email.basic ", evt);
                 if(evt.headerName == 'xpl-cmnd' && wt.validBasicSchema(evt.body)) wt.sendEmail(evt.body);
         }); 
         
-        xpl.on("xpl:email.config", function(evt) {
+        /*xpl.on("xpl:email.config", function(evt) {
 		console.log("Receive message email.config ", evt);
                 if(evt.headerName == 'xpl-cmnd' && wt.validConfigSchema(evt.body)) wt.writeConfig(evt.body);
-        }); 
-
-        xpl.on("xpl:email.request", function(evt) {
-		console.log("Receive message email.request ", evt);
-                if(evt.headerName == 'xpl-cmnd') wt.readConfig();
-        });
+        }); */
 });
 
