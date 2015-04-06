ac5c9b0bd5dd68e6a00b12dcb74747ae2159958d
Mathieu Garcia <mgarcia@wiseflat.com>
2015-04-07 01:40:48 +0200

    evo - configuration de email.config

--- index.js
+++ index.js
@@ -25,16 +25,12 @@ wt.init(function(error, xpl) {
                 wt.sendConfig();
         }, 60 * 1000);
         
-        xpl.on("xpl:email.request", function(evt) {
-                if(evt.headerName == 'xpl-cmnd') wt.readConfig();
-        });
-        
         xpl.on("xpl:email.basic", function(evt) {
-		if(evt.headerName == 'xpl-cmnd') wt.sendEmail(evt.body);
+		if(evt.headerName == 'xpl-cmnd') wt.sendEmail(evt.header.target, evt.body);
         }); 
         
         xpl.on("xpl:email.config", function(evt) {
-		if(evt.headerName == 'xpl-cmnd') wt.writeConfig(evt.body);
+		if(evt.headerName == 'xpl-cmnd') wt.writeConfig(evt.header.target, evt.body);
         });
 });
 
