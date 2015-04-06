e46e53bb56e5b66008a5899f62834c2afcb1d51e
Mathieu Garcia <mgarcia@wiseflat.com>
2015-04-06 16:03:23 +0200

    evo - log

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ wt.init(function(error, xpl) {
         // Send every minutes an xPL status message 
         setInterval(function(){
                 wt.sendConfig();
-		//wt.sendBasic();
         }, 60 * 1000);
         
         xpl.on("xpl:email.request", function(evt) {
@@ -31,14 +30,10 @@ wt.init(function(error, xpl) {
         });
         
         xpl.on("xpl:email.basic", function(evt) {
-		//console.log(evt);
-                //if(evt.headerName == 'xpl-cmnd' && wt.validBasicSchema(evt.body)) wt.sendEmail(evt.body);
 		if(evt.headerName == 'xpl-cmnd') wt.sendEmail(evt.body);
         }); 
         
         xpl.on("xpl:email.config", function(evt) {
-		//console.log("Receive message email.config ", evt);
-                //if(evt.headerName == 'xpl-cmnd' && wt.validConfigSchema(evt.body)) wt.writeConfig(evt.body);
 		if(evt.headerName == 'xpl-cmnd') wt.writeConfig(evt.body);
         });
 });
